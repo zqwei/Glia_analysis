7f679363b9f25db79add79b9c8842f48fd7f3daf
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-02-07 14:17:57 -0500

    update df/f computation

--- Processing/DFF_visualization.py
+++ Processing/DFF_visualization.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings('ignore')
 import fish_proc.wholeBrainDask.cellProcessing_single_WS as fwc
+from fish_proc.utils.fileio import make_tarfile, chmod
 import dask.array as da
 import pandas as pd
 df = pd.read_csv('../Processing/data_list.csv')
@@ -20,7 +21,7 @@ for ind, row in df.iterrows():
         continue
     brain_map = np.load(save_root+'Y_ave.npy')
     num_plane = brain_map.shape[0]
-    if os.path.exists(save_root+'brain_map.png'):
+    if not os.path.exists(save_root+'brain_map.png'):
         fig, ax = plt.subplots(4, num_plane//4+1, figsize=(40, 10))
         ax = ax.flatten()
         for n, _ in enumerate(brain_map):
@@ -29,27 +30,23 @@ for ind, row in df.iterrows():
         plt.savefig(save_root+'brain_map.png')
         plt.close()
     
-    ## if recompute dFF
-    # _ = np.load(save_root+'cell_raw_dff_sparse.npz', allow_pickle=True)
-    # A = _['A'].astype('float')
-    # F_ = _['F'].astype('float')
-    # A_loc = _['A_loc']
-    # _ = None
-
-    # # first remove components with low signals
-    # valid_cell = F_.max(axis=-1)>20
-
-    # A = A[valid_cell]
-    # A_loc = A_loc[valid_cell]
-    # F_ = F_[valid_cell]
-
-    # F_dask = da.from_array(F_, chunks=('auto', -1))
-    # baseline_ = da.map_blocks(fwc.baseline, F_dask, dtype='float', window=400, percentile=20, downsample=10).compute()
-    # dFF = F_/baseline_-1
-
-    # invalid_ = (dFF.max(axis=-1)>5) | (np.isnan(dFF.max(axis=-1))) | (baseline_.min(axis=-1)<=0)
-
-    # np.savez(save_root+'cell_dff.npz', A=A[~invalid_].astype('float16'), A_loc=A_loc[~invalid_], dFF=dFF[~invalid_].astype('float16'))
+    # if recompute dFF
+    if not os.path.exists(save_root+'cell_dff.npz'):
+        _ = np.load(save_root+'cell_raw_dff_sparse.npz', allow_pickle=True)
+        A = _['A'].astype('float')
+        F_ = _['F'].astype('float')
+        A_loc = _['A_loc']
+        _ = None
+        # first remove components with low signals
+        valid_cell = F_.max(axis=-1)>20
+        A = A[valid_cell]
+        A_loc = A_loc[valid_cell]
+        F_ = F_[valid_cell]
+        F_dask = da.from_array(F_, chunks=('auto', -1))
+        baseline_ = da.map_blocks(fwc.baseline, F_dask, dtype='float', window=400, percentile=20, downsample=10).compute()
+        dFF = F_/baseline_-1
+        invalid_ = (dFF.max(axis=-1)>5) | (np.isnan(dFF.max(axis=-1))) | (baseline_.min(axis=-1)<=0)
+        np.savez(save_root+'cell_dff.npz', A=A[~invalid_].astype('float16'), A_loc=A_loc[~invalid_], dFF=dFF[~invalid_].astype('float16'))
     
     _ = np.load(save_root+'cell_dff.npz')
     A=_['A'].astype('float')
@@ -64,7 +61,7 @@ for ind, row in df.iterrows():
         cx, cy = A_ext[z, x:x+100, y:y+100].shape
         A_ext[z, x:x+100, y:y+100]=np.maximum(A_ext[z, x:x+100, y:y+100], _[:cx, :cy])
     
-    if os.path.exists(save_root+'components.png'):
+    if not os.path.exists(save_root+'components.png'):
         fig, ax = plt.subplots(4, A_ext.shape[0]//4+1, figsize=(40, 10))
         ax = ax.flatten()
         for n, _ in enumerate(A_ext):
@@ -73,4 +70,5 @@ for ind, row in df.iterrows():
             ax[n].axis('off')
         plt.savefig(save_root+'components.png')
         plt.close()
-    
+
+#    chmod(save_root, mode='0775')
