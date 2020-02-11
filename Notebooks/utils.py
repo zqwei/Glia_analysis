7b05815e34c58e746de8f1e87965ad09fd2fbcee
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-02-10 19:11:22 -0500

    add function to find cells response during motor and sensory epochs

--- Notebooks/utils.py
+++ Notebooks/utils.py
@@ -7,8 +7,33 @@ import fish_proc.wholeBrainDask.cellProcessing_single_WS as fwc
 import dask.array as da
 import pandas as pd
 from glob import glob
+from tqdm import tqdm
 
 
 def ep2frame(camtrig, thres=3.8):
     arr_ = (camtrig>thres).astype('int')
     return np.where((arr_[:-1]-arr_[1:])==-1)[0]+1
+
+
+def WNtest(zdFF, lags=30):
+    from statsmodels.stats.diagnostic import acorr_ljungbox
+    num_cell = zdFF.shape[0]
+    valid_ = np.zeros(num_cell).astype('bool')
+    for n_cell in tqdm(range(num_cell)):
+        _, p = acorr_ljungbox(zdFF[n_cell], lags=lags)
+        if (p<.05).sum()>0:
+            valid_[n_cell]=True
+    return valid_
+
+
+def layer_corr(zdFF, A_loc, corr_thres=0.2, corr_size=10):
+    from scipy.stats import spearmanr
+    num_cell = zdFF.shape[0]
+    num_z = A_loc[:,0].max()+1
+    valid_ = np.zeros(num_cell).astype('bool')
+    for nz in tqdm(range(num_z)):
+        nz_list = np.where(A_loc[:,0]==nz)[0]
+        corr_, p_ = spearmanr(zdFF[nz_list], axis=1)
+        valid_thres = ((p_<0.05).sum(axis=-1)>corr_size) & ((np.abs(corr_)>corr_thres).sum(axis=-1)>corr_size)
+        valid_[nz_list[valid_thres]]=True
+    return valid_
