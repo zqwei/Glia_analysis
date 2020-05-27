88a1d203932bda27a16fafed51d324060f1b530d
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-05-26 22:51:11 -0400

    update factor analysis

--- Notebooks/factor.py
+++ Notebooks/factor.py
@@ -5,7 +5,7 @@ from factor_analyzer import Rotator
 
 def factor_(dFF, n_c=10, noise_thres=0.8):
     dFFz=zscore(dFF, axis=-1)
-    FA_=FactorAnalysis(n_components=n_c, svd_method='randomized',random_state=None)
+    FA_=FactorAnalysis(n_components=n_c, svd_method='randomized', random_state=None, iterated_power=7)
     # first fitting with all neurons
     FA_.fit(dFFz.T)
     valid_cell=FA_.noise_variance_<noise_thres
