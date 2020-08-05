decd0769857f1c348d046d01364a33d0780b4a17
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-08-05 10:36:05 -0400

    fix error grammar

--- Notebooks/brain_seg_dynamics.py
+++ Notebooks/brain_seg_dynamics.py
@@ -4,7 +4,7 @@ from scipy.stats import spearmanr
 df = pd.read_csv('../Processing/data_list.csv')
 len_ = len(df)
 
-for file_id in range(13, len_+1)
+for file_id in range(13, len_+1):
     row = df.iloc[file_id]
     save_root = row['save_dir']+'/'
     brain_map = np.load(save_root+'Y_ave.npy').astype('float').squeeze()
