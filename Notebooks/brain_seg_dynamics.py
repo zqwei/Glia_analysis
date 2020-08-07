3f91ad1f36c44a43c8dda751ff7bf6bacabefd73
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-08-06 22:35:04 -0400

    update matplotlib backend

--- Notebooks/brain_seg_dynamics.py
+++ Notebooks/brain_seg_dynamics.py
@@ -1,12 +1,14 @@
+import matplotlib
+matplotlib.use('Agg')
 from utils import *
-from brain_seg import brain_seg_factor
-from scipy.stats import spearmanr
+from brain_seg import brain_seg_factor, brain_layer_seg_factor
+
 df = pd.read_csv('../Processing/data_list.csv')
 len_ = len(df)
 
-for file_id in range(13, len_+1):
+for file_id in range(10, 11):
     row = df.iloc[file_id]
     save_root = row['save_dir']+'/'
-    brain_map = np.load(save_root+'Y_ave.npy').astype('float').squeeze()
+    brain_layer_seg_factor(row, t_min=5000, t_max=30000, l_thres_=0.5, n_thres = 0.8)
     brain_seg_factor(row, t_min=5000, t_max=30000, num_cluster=300, l_thres_=0.5, n_thres = 0.8)
 
