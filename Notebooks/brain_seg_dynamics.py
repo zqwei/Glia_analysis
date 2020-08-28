cc898453f3b572b5fed3a14ad5f8db0cc628babd
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-08-27 23:42:07 -0400

    update brain_seg_dynamics

--- Notebooks/brain_seg_dynamics.py
+++ Notebooks/brain_seg_dynamics.py
@@ -6,7 +6,7 @@ from brain_seg import brain_seg_factor, brain_layer_seg_factor
 df = pd.read_csv('../Processing/data_list.csv')
 len_ = len(df)
 
-for file_id in range(5, 10):
+for file_id in range(32, 33):
     row = df.iloc[file_id]
     save_root = row['save_dir']+'/'
     brain_layer_seg_factor(row, t_min=5000, t_max=30000, l_thres_=0.5, n_thres = 0.8)
