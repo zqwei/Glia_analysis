08e5df5f299467740f0dcce21764945ad1cdbf59
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-08-31 00:06:12 -0400

    skip unprocessed fiel

--- Notebooks/brain_seg_dynamics.py
+++ Notebooks/brain_seg_dynamics.py
@@ -10,6 +10,8 @@ for ind, row in df.iterrows():
     save_root = row['save_dir']+'/'
     if os.path.exists(save_root+'brain_seg_factors.npz'):
         continue
+    if not os.path.exists(save_root+'cell_dff.npz'):
+        continue
     brain_layer_seg_factor(row, t_min=5000, t_max=30000, l_thres_=0.5, n_thres = 0.8)
     brain_seg_factor(row, t_min=5000, t_max=30000, num_cluster=300, l_thres_=0.5, n_thres = 0.8)
 
