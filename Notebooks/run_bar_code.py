7661729e30fadcd61b104af7b2bef719b26b9933
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-09-09 11:05:48 -0400

    rerun the bar code over fishes

--- Notebooks/run_bar_code.py
+++ Notebooks/run_bar_code.py
@@ -1,14 +1,15 @@
 from single_cell_type_cluster import *
 
 df = pd.read_csv('../Processing/data_list.csv')
+# df = df[:13]
 
 for ind, row in df.iterrows():
     save_root = row['save_dir']+'/'
-    if not (row['taskType']=='LGNGGU'):
+    if not 'LG_vs_' in row['taskType']:
         continue
     if not os.path.exists(save_root+'brain_seg_factors.npz'):
         continue
 #     if os.path.exists(save_root+'cell_active_pulse_stats.npz'):
 #         continue  
-    print(save_root)
+    print(save_root, row['taskType'])
     bar_code(row)
