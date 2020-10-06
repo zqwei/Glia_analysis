4e32ee02d55e559ad3c72bf0250bc3ace49e8b19
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-10-05 22:42:10 -0400

    update bar code for brain states

--- Notebooks/run_bar_code.py
+++ Notebooks/run_bar_code.py
@@ -1,15 +1,19 @@
 from sensory_motor_bar_code import *
 from brain_state_bar_code import *
+from pathlib import Path
 
 df = pd.read_csv('../Processing/data_list.csv')
-# df = df[:13]
 
 for ind, row in df.iterrows():
     save_root = row['save_dir']+'/'
-    if not 'LG_vs_' in row['taskType']:
+    if not 'LG' in row['taskType']:
         continue
     if not os.path.exists(save_root+'brain_seg_factors.npz'):
         continue
     print(row['save_dir'])
-    if not os.path.exists(save_root+'cell_type_stats_motor.npz'):
+    if not os.path.exists(row['save_dir']+'/'+'sensory_motor_bar_code.done'):
         sensory_motor_bar_code(row)
+        Path(row['save_dir']+'/'+'sensory_motor_bar_code.done').touch()
+    if not os.path.exists(row['save_dir']+'/'+'brain_state_bar_code.done'):
+        brain_state_bar_code(row)
+        Path(row['save_dir']+'/'+'brain_state_bar_code.done').touch()
