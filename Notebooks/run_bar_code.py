cf0695f391b32965194cce0e353f842f610a17b5
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-11-21 22:09:11 -0500

    update processing files

--- Notebooks/run_bar_code.py
+++ Notebooks/run_bar_code.py
@@ -1,25 +1,34 @@
 from sensory_motor_bc import *
 from brain_state_bcr import *
+from brain_state_rl_bcr import *
 from multi_vel_bc import *
 from pathlib import Path
 
 df = pd.read_csv('../Processing/data_list_in_analysis.csv')
 
-# for ind, row in df.iterrows():
-#     save_root = row['save_dir']+'/'
-#     if not 'LG' in row['taskType']:
-#         continue
-#     print(row['save_dir'])
-#     if ind<12:
-#         continue
-#     sensory_motor_bar_code(row)
-#     brain_state_bar_code_raw(row)
-
-
 for ind, row in df.iterrows():
     save_root = row['save_dir']+'/'
-    if not 'LG_vs_NGGUmultivel' in row['taskType']:
+    if 'replay' in row['taskType']:
         continue
+#     if ind<15:
+#         continue
     print(row['save_dir'])
-    multi_vel_bar_code(row)
+#     sensory_motor_bar_code(row)
+    brain_state_bar_code_raw(row)
+
+
+# for ind, row in df.iterrows():
+#     save_root = row['save_dir']+'/'
+#     if not 'LG_vs_NGGUmultivel' in row['taskType']:
+#         continue
+#     print(row['save_dir'])
+#     multi_vel_bar_code(row)
+
+
+# for ind, row in df.iterrows():
+#     save_root = row['save_dir']+'/'
+#     if not 'MG_vs_replayGU' in row['taskType']:
+#         continue
+#     print(row['save_dir'])
+#     brain_state_rl_bar_code_raw(row)
     
