54c0d484e15b0de3c64904e3b736bea3c53de2c3
Ziqiang Wei <weiz@janelia.hhmi.org>
2020-11-21 22:04:18 -0500

    fix error in dff to zarr

--- Notebooks/cell_dff_zarr.py
+++ Notebooks/cell_dff_zarr.py
@@ -23,5 +23,6 @@ def cell_dff_zarr(row):
 if __name__ == "__main__":
     df = pd.read_csv('../Processing/data_list_in_analysis.csv')
     for ind, row in df.iterrows():
+        save_root = row['save_dir']+'/'
         if not os.path.exists(save_root+'cell_dff.zarr'):
             cell_dff_zarr(row)
