08d6a663f6d1e1de50a3ea4ce426daf18e037e6a
Ziqiang Wei <weiz@janelia.hhmi.org>
2021-03-03 00:21:37 -0500

    update brain segmentation file

--- Notebooks/brain_seg_dynamics.py
+++ Notebooks/brain_seg_dynamics.py
@@ -3,7 +3,7 @@ matplotlib.use('Agg')
 from utils import *
 from brain_seg import brain_seg_factor, brain_layer_seg_factor
 
-df = pd.read_csv('../Processing/data_list_in_analysis.csv')
+df = pd.read_csv('../Processing/data_list_in_analysis_osc_curated.csv')
 
 for ind, row in df.iterrows():
     save_root = row['save_dir']+'/'
