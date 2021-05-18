396f63236ebc1634b26022dd11d03fc7ed7798f8
Ziqiang Wei <weiz@janelia.hhmi.org>
2021-05-18 10:28:01 -0400

    add factor, ev analysis to utils

--- Notebooks/utils.py
+++ Notebooks/utils.py
@@ -8,7 +8,81 @@ import pandas as pd
 from glob import glob
 from tqdm import tqdm
 import seaborn as sns
-sns.set(font_scale=2, style='ticks')
+from scipy.stats import ranksums, wilcoxon
+from sklearn.cluster import KMeans
+from statsmodels.multivariate.manova import MANOVA
+from scipy.stats import spearmanr, linregress, wilcoxon
+from matplotlib.colors import ListedColormap
+from sklearn.cluster import OPTICS, DBSCAN
+from sklearn.decomposition import FactorAnalysis
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from scipy.stats import spearmanr
+from scipy.stats import zscore
+sns.set(font_scale=1.5, style='ticks')
+
+
+def FA_ev(X, n_c=10):
+    X = zscore(X, axis=-1)
+    FA_=FactorAnalysis(n_components=n_c, svd_method='randomized', random_state=None, iterated_power=7)
+    FA_.fit(X.T)
+    coeff_= FA_.components_
+    x_new = (FA_.transform(X.T).dot(coeff_)).T
+    return 1-((X-x_new)**2).mean(axis=-1)
+
+
+def moving_average(x, win, axis=0):
+    return pd.DataFrame(x).rolling(win, win_type='boxcar', center=False, min_periods=1, axis=axis).mean().to_numpy()
+
+
+def plt_error(x, Y, ax, axis=0, sem=False, color='k', linewidth=2):
+    n = Y.shape[axis]
+    mean_ = np.nanmean(Y, axis=axis)
+    error = np.nanstd(Y, axis=axis)
+    if sem:
+        error = error/np.sqrt(n)
+    ax.plot(x, mean_, '-', color=color, linewidth=linewidth)
+    ax.fill_between(x, mean_-error, mean_+error, alpha=0.5, color=color)
+    
+    
+def weight_proj(weight, axis=0):
+    weight_max = weight.max(axis)
+    weight_min = weight.min(axis)
+    w_ = weight_max.copy()
+    w_[np.abs(weight_max)<np.abs(weight_min)] = weight_min[np.abs(weight_max)<np.abs(weight_min)]
+    w_ = np.ma.array (w_, mask=(w_==0))
+    return w_
+
+
+def load_masked_data(row, mask_thres=2):
+    save_root = row['save_dir']+'/'
+    _ = np.load(save_root+'cell_dff.npz', allow_pickle=True)
+    A = _['A']
+    A_loc = _['A_loc']
+    dFF = _['dFF'].astype('float')
+    num_dff = _['dFF'].shape[-1]
+    _ = None
+    brain_map = np.load(save_root+'Y_ave.npy').astype('float').squeeze()
+    A_center = np.load(save_root+'cell_center.npy')
+    A_center_grid = np.round(A_center).astype('int')
+    cells_in_mask = []
+    for n_layer in range(brain_map.shape[0]):
+        layer_ = A_center[:, 0]==n_layer
+        cell_ids = np.where(layer_)[0]
+        mask_ = brain_map[n_layer]>mask_thres
+        y = A_center_grid[cell_ids, 2]
+        x = A_center_grid[cell_ids, 1]
+        x_max, y_max = mask_.shape
+        num_cells = len(cell_ids)
+        in_mask_ = np.zeros(num_cells).astype('bool')
+        for n in range(num_cells):
+            if (x[n]<x_max) and (y[n]<y_max):
+                in_mask_[n] = mask_[x[n], y[n]]
+        cells_in_mask.append(cell_ids[in_mask_])
+    cells_in_mask = np.concatenate(cells_in_mask)
+    A_center = A_center[cells_in_mask]
+    dFF = dFF[cells_in_mask]
+    return dFF, A_center
 
 
 def ep2frame(camtrig, thres=3.8):
