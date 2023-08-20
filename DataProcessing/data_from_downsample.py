83b31b14b1eae9ed646002bd0675f79442a8ea4c
weiz <weiz@janelia.hhmi.org>
2023-08-20 10:41:41 -0400

    update data processing for ACh

--- DataProcessing/data_from_downsample.py
+++ DataProcessing/data_from_downsample.py
@@ -7,10 +7,13 @@ import dask.array as da
 import os
 import pandas as pd
 from glob import glob
-sub_str = 'im_CM._dff'
-temp_str = re.compile(sub_str)
+from tqdm import tqdm
+# sub_str = 'im_CM._dff'
+# temp_str = re.compile(sub_str)
 # df = pd.read_csv('../Datalists/data_list_in_analysis_slimmed_v4.csv')
-df = pd.read_csv('../Datalists/data_list_in_analysis_NE_v3.csv')
+# df = pd.read_csv('../Datalists/data_list_in_analysis_NE_v3.csv')
+# df = pd.read_csv('../Datalists/data_list_in_analysis_DA_v1.csv')
+df = pd.read_csv('../Datalists/data_list_in_analysis_ACh_v0.csv')
 
 def imread(v, baseline=100):
     # img = File(v,'r')['func'][()]
@@ -21,21 +24,17 @@ def imread(v, baseline=100):
     return img
 
 for n, row in df.iterrows():
-    if n <12:
-        continue
     im_dir = row['im_volseg']
-    if temp_str.search(im_dir) is None:
-        # print(im_dir)
-        # print('not downsample data skip')
-        continue
+    # if temp_str.search(im_dir) is None:
+    #     # print(im_dir)
+    #     # print('not downsample data skip')
+    #     continue
     save_root = row['save_dir']
     if '/' not in save_root:
-        # print(save_root)
-        # print('invalid save location')
+        print(save_root)
+        print('invalid save location')
         continue
     if os.path.exists(save_root+'cell_center.npy'):
-        # print(save_root)
-        # print('already exists')
         continue
     if not os.path.exists(save_root):
         os.makedirs(save_root)
@@ -47,15 +46,17 @@ for n, row in df.iterrows():
     np.save(save_root+'Y_ave.npy', brain_map)
     
     brain_map = np.load(save_root+'Y_ave.npy').astype('float')
-    dff_data = da.from_zarr(im_dir).compute().astype('float16')
     thres_ = np.percentile(brain_map, 60)
     mask = brain_map>thres_
-    dFF = dff_data[:,mask].T # cells x time
-    np.savez(save_root+'cell_dff.npz', dFF=dFF.astype('float16'))
-
     z, x, y = np.where(mask)
+    dff_data = da.from_zarr(im_dir).compute().astype('float16')
     A_center = np.hstack([z[None, :].T, x[None, :].T, y[None, :].T])
+    # dFF = np.zeros((len(z), dff_data.shape[0]))
+    # for n in tqdm(range(len(z))):
+    #     dFF[n] = dff_data[:, z[n], x[n], y[n]].compute()
+    dFF = dff_data[:, mask].T
     np.save(save_root+'cell_center.npy', A_center)
+    np.savez(save_root+'cell_dff.npz', dFF=dFF.astype('float16'))
 
     dff_data = None
     dFF = None
